2fe0fdad8e76bf60a17a349bd3a810508f24b52a
cathyroney <cag8@hawaii.edu>
2026-06-17 02:58:32 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2867
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red174\green0\blue75;}
+{\*\expandedcolortbl;;\cssrgb\c74510\c9412\c36471;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sa80\partightenfactor0
+
+\f0\fs28 \cf2 \expnd0\expndtw0\kerning0
+print("Hello from Binder!")}
